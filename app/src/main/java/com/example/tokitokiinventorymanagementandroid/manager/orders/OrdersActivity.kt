d9b281aea9a77f78e656114514702fd22aab83f2
Soljira <soljiragit@gmail.com>
2025-01-29 21:55:25 +0800

    feat: Implement Bottom Navigation Bar and Navigation

--- app/src/main/java/com/example/tokitokiinventorymanagementandroid/manager/orders/OrdersActivity.kt
+++ app/src/main/java/com/example/tokitokiinventorymanagementandroid/manager/orders/OrdersActivity.kt
@@ -1,12 +1,29 @@
 package com.example.tokitokiinventorymanagementandroid.manager.orders
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tokitokiinventorymanagementandroid.R
+import com.example.tokitokiinventorymanagementandroid.helpers.BottomNavigationInitialization
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class OrdersActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.manager_orders_customer_orders)
+
+        // Bottom Navigation Bar DO NOT TOUCH
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        BottomNavigationInitialization.setupBottomNavigation(this, bottomNavigationView)
+        bottomNavigationView.selectedItemId = R.id.navbar_manager_orders
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bottomNavigationView.selectedItemId = R.id.navbar_manager_orders
     }
 }
--- app/src/main/java/com/example/tokitokiinventorymanagementandroid/manager/recipes/RecipeActivity.kt
+++ app/src/main/java/com/example/tokitokiinventorymanagementandroid/manager/recipes/RecipeActivity.kt
@@ -1,15 +1,27 @@
 package com.example.tokitokiinventorymanagementandroid.manager.recipes
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.tokitokiinventorymanagementandroid.R
+import com.example.tokitokiinventorymanagementandroid.helpers.BottomNavigationInitialization
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class RecipeActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.manager_recipe_overview)
+
+        // Bottom Navigation Bar DO NOT TOUCH
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        BottomNavigationInitialization.setupBottomNavigation(this, bottomNavigationView)
+        bottomNavigationView.selectedItemId = R.id.navbar_manager_recipe
 
         val btnAddRecipe = findViewById<Button>(R.id.addRecipeBtn)
         val btnEditRecipe = findViewById<Button>(R.id.editBtn)
@@ -21,5 +33,11 @@ class RecipeActivity : AppCompatActivity() {
         btnEditRecipe.setOnClickListener {
             startActivity(Intent(this, EditRecipe::class.java))
         }
+    }
+
+    // THIS FUNCTION ENSURES THAT THE APPROPRIATE ICON IS CHECKED EVEN WHEN YOU PRESS THE BACK BUTTON
+    override fun onResume() {
+        super.onResume()
+        bottomNavigationView.selectedItemId = R.id.navbar_manager_recipe
     }
 }
