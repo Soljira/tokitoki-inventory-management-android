ef5a78d3f531f2f577f7964573e3b63584844e8f
Princeee <@Floppyyy1>
2025-02-07 18:11:45 +0800

    delivery_request.xml supplier_profile.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.cardview.v7)
+    implementation(libs.constraint.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
